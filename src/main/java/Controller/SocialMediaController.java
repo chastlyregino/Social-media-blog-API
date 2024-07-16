6ef9d9251794c4a5988ded36bf91421262f022b5
Finn Chastly Manapat <chastly_regino@icloud.com>
2024-07-16 22:59:43 +0000

    SocialMediaController.java template and DAOs creation

--- src/main/java/Controller/SocialMediaController.java
+++ src/main/java/Controller/SocialMediaController.java
@@ -16,7 +16,15 @@ public class SocialMediaController {
      */
     public Javalin startAPI() {
         Javalin app = Javalin.create();
-        app.get("example-endpoint", this::exampleHandler);
+        app.post("/register", this::postRegistrationHandler);
+        app.post("/login", this::postLoginHandler);
+        app.post("/messages", this::postMessagesHandler);
+        app.get("/messages", this::getMessagesHandler);
+        app.get("/messages/{message_id}", this::getMessagesUsingIdHandler);
+        app.delete("/messages/{message_id}", this::deleteMessagesUsingIdHandler);
+        app.patch("/messages/{message_id}", this::patchMessagesUsingIdHandler);
+        app.get("/accounts/{account_id}/messages", this::getMessagesOfUser);
+        app.start(8080);
 
         return app;
     }
@@ -25,9 +33,35 @@ public class SocialMediaController {
      * This is an example handler for an example endpoint.
      * @param context The Javalin Context object manages information about both the HTTP request and response.
      */
-    private void exampleHandler(Context context) {
+    private void postRegistrationHandler(Context context) {
         context.json("sample text");
     }
 
+    private void postLoginHandler(Context context) {
+        context.json("sample text");
+    }
 
+    private void postMessagesHandler(Context context) {
+        context.json("sample text");
+    }
+
+    private void getMessagesHandler(Context context) {
+        context.json("sample text");
+    }
+
+    private void getMessagesUsingIdHandler(Context context) {
+        context.json("sample text");
+    }
+
+    private void deleteMessagesUsingIdHandler(Context context) {
+        context.json("sample text");
+    }
+
+    private void patchMessagesUsingIdHandler(Context context) {
+        context.json("sample text");
+    }
+
+    private void getMessagesOfUser(Context context) {
+        context.json("sample text");
+    }
 }
